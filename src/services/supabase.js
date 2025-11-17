d0fabbb71350d6add0897b70e965b962e132b1eb
Marouane Dagana <136081716+daganoo@users.noreply.github.com>
2025-11-17 15:41:07 +0100

    Added React Query for data fetching and state management

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
+export const supabaseUrl = "https://wknjywipwyphmvivduso.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indrbmp5d2lwd3lwaG12aXZkdXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0MjkyNzgsImV4cCI6MjA3ODAwNTI3OH0.Gsg6fl7pkgjp78t9Hh5pbZP6WDl8osQWGhkB_Lw6GnY";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
